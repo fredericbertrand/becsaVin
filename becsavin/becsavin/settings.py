c95c7f6af1f81f775bc1725bc0d46561d8d96f87
elineau <elineau.sylvain@gmail.com>
2020-10-13 11:00:08 +0200

    creation compte admin + app_account

--- becsavin/becsavin/settings.py
+++ becsavin/becsavin/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main_app.apps.MainAppConfig",
+    # "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
